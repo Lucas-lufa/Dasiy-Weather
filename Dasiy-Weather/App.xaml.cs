a8b4cf64ee7ddf2df5091380f7614015c97767b5
LUCAS FADERSEN <V092953@tafe.wa.edu.au>
2023-09-19 07:00:58 +0800

    Add project files.

---
+++ Dasiy-Weather/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Dasiy_Weather;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Dasiy-Weather/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Dasiy_Weather;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
