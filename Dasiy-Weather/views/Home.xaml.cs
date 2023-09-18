3c4f9b632c2791a2265514960d41e65c6cf919be
LUCAS FADERSEN <V092953@tafe.wa.edu.au>
2023-09-19 07:14:29 +0800

    feat: add pages

---
+++ Dasiy-Weather/views/Home.xaml.cs
@@ -0,0 +1,9 @@
+namespace Dasiy_Weather.views;
+
+public partial class Home : ContentPage
+{
+	public Home()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Dasiy-Weather/views/Settings.xaml.cs
@@ -0,0 +1,9 @@
+namespace Dasiy_Weather.views;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+}
