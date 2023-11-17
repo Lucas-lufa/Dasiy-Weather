b90c854fcfb105426a76b5115cfbc4bdba24154c
LUCAS FADERSEN <V092953@tafe.wa.edu.au>
2023-11-17 14:26:20 +0800

    added to home view

--- Dasiy-Weather/views/Home.xaml.cs
+++ Dasiy-Weather/views/Home.xaml.cs
@@ -5,5 +5,13 @@ public partial class Home : ContentPage
 	public Home()
 	{
 		InitializeComponent();
-	}
+		displayFav();
+    }
+
+	private async void displayFav()
+	{
+        string URL = Preferences.Default.Get("fav", "to-deside");
+        LocationWeather locationWeather = await APIService.GetLocationWeather(URL);
+		output.Text = locationWeather.ToString();
+    }
 }
