d09087a0eb826eb9c57485fe5b730a24a4e661df
LUCAS FADERSEN <V092953@tafe.wa.edu.au>
2023-11-20 13:20:46 +0800

    if no preferences will go to search page

--- Dasiy-Weather/views/Home.xaml.cs
+++ Dasiy-Weather/views/Home.xaml.cs
@@ -12,7 +12,20 @@ public partial class Home : ContentPage
 	private async void displayFav()
 	{
         string URL = Preferences.Default.Get("fav", "to-deside");
+		if (URL == "to-deside")
+		{
+            changeScreen();
+        }
+        else { 
         LocationWeather locationWeather = await service.GetLocationWeather(URL);
 		output.Text = locationWeather.ToString();
+        }
+    }
+
+    private void changeScreen()
+	{
+        //	Application.Current.MainPage.Navigation.PushModalAsync(new SettingsPage(), true);
+        Application.Current.MainPage.Navigation.PushModalAsync(new Search(), true);
+        //https://jinoh.co/net-maui-adding-a-new-page-and-redirect/
     }
 }
